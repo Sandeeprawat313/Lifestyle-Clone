2af116287e8b8fccef5889597667e8cf443532dc
rahul <rkrahul8181@gmail.com>
2022-06-18 10:47:01 +0530

    updated

--- cart.js
+++ cart.js
@@ -34,7 +34,7 @@ function displayCart(data){
         <p>ProductId : ${ele.Productid}</p>
     </div>
     </div>
-    <h2><p class="delsym"><i class="fa-solid fa-blinds"></i>||||</p><span class="delevery" style="font-weight:normal;">Delivery in   </span> <span> Enter pincode above</span><span class="qty">Qty:1</span></h2> ` ;
+    <h2><p class="delsym"><i class="fa-solid fa-blinds"></i></p><span class="delevery" style="font-weight:normal;">Delivery in   </span> <span> Enter pincode above</span><span class="qty">Qty:1</span></h2> ` ;
     let delRem_div = document.createElement('div');
     let remove = document.createElement('h5');
     remove.innerText ="Remove";
